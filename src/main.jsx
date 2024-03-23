70b89a2e2d9e5bc20f1535c11a98ab925461c058
Mohammed Atef <atefmohammed@std.mans.edu.eg>
2024-03-23 16:02:26 +0200

    fix router

--- src/main.jsx
+++ src/main.jsx
@@ -9,32 +9,38 @@ import ProtectedRoute from "./pages/ProtectedRoute";
 import ProtectedSignRoute from "./pages/ProtectedSignRoute";
 import Register from "./pages/Register";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Dashboard />
+        </ProtectedRoute>
+      ),
+    },
+    {
+      path: "/register",
+      element: (
+        <ProtectedSignRoute>
+          <Register />
+        </ProtectedSignRoute>
+      ),
+    },
+    {
+      path: "/login",
+      element: (
+        <ProtectedSignRoute>
+          <Login />
+        </ProtectedSignRoute>
+      ),
+    },
+  ],
   {
-    path: "/",
-    element: (
-      <ProtectedRoute>
-        <Dashboard />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: "/register",
-    element: (
-      <ProtectedSignRoute>
-        <Register />
-      </ProtectedSignRoute>
-    ),
-  },
-  {
-    path: "/login",
-    element: (
-      <ProtectedSignRoute>
-        <Login />
-      </ProtectedSignRoute>
-    ),
-  },
-]);
+    // Pass the base name to the router
+    basename: "/Authentication",
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
