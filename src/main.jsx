8408ccf29e991f2166111198c40a07eff52ab223
Mohammed Atef <atefmohammed@std.mans.edu.eg>
2024-03-24 19:12:45 +0200

    add: hash routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import "./index.css";
 import Dashboard from "./pages/Dashboard";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -11,50 +11,44 @@ import ProtectedSignRoute from "./pages/ProtectedSignRoute";
 import Register from "./pages/Register";
 import UpdatePassword from "./pages/UpdatePassword";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: (
-        <ProtectedRoute>
-          <Dashboard />
-        </ProtectedRoute>
-      ),
-    },
-    {
-      path: "/register",
-      element: (
-        <ProtectedSignRoute>
-          <Register />
-        </ProtectedSignRoute>
-      ),
-    },
-    {
-      path: "/login",
-      element: (
-        <ProtectedSignRoute>
-          <Login />
-        </ProtectedSignRoute>
-      ),
-    },
-    {
-      path: "/forgotpassword",
-      element: (
-        <ProtectedSignRoute>
-          <ForgotPassword />
-        </ProtectedSignRoute>
-      ),
-    },
-    {
-      path: "/updatepassword",
-      element: <UpdatePassword />,
-    },
-  ],
+const router = createHashRouter([
   {
-    // Pass the base name to the router
-    basename: "/Authentication",
-  }
-);
+    path: "/",
+    element: (
+      <ProtectedRoute>
+        <Dashboard />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/register",
+    element: (
+      <ProtectedSignRoute>
+        <Register />
+      </ProtectedSignRoute>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <ProtectedSignRoute>
+        <Login />
+      </ProtectedSignRoute>
+    ),
+  },
+  {
+    path: "/forgotpassword",
+    element: (
+      <ProtectedSignRoute>
+        <ForgotPassword />
+      </ProtectedSignRoute>
+    ),
+  },
+  {
+    path: "/updatepassword",
+    element: <UpdatePassword />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
